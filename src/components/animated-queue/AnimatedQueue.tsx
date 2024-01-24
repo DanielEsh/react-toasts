a82b12558ad5125ceffa11a841ac9a31e29090d2
Daniel <daniilterawars@gmail.com>
2024-01-24 07:51:11 +0300

    feat: animated queue

---
+++ src/components/animated-queue/AnimatedQueue.tsx
@@ -0,0 +1,60 @@
+import { AnimatedListItem } from '../list/AnimatedListItem.tsx'
+import { useQueue } from '../../use-queue.ts' // Update the path as needed
+
+function generateId() {
+  return Date.now()
+}
+
+interface ListedElement {
+  id: number
+  name: string
+}
+
+export const AnimatedQueue = () => {
+  const {
+    state: items,
+    queue,
+    add: addItem,
+    update: updateItems,
+    cleanQueue,
+  } = useQueue<ListedElement>({
+    initialValues: [
+      { id: 1, name: 'name1' },
+      { id: 2, name: 'name2' },
+    ],
+    limit: 3, // Adjust the limit as needed
+  })
+
+  const handleAddItem = () => {
+    addItem({
+      id: generateId(),
+      name: 'new element',
+    })
+  }
+
+  const deleteItem = (index: number) => {
+    updateItems((prevItems) => {
+      const newItems = [...prevItems]
+      newItems.splice(index, 1)
+      return newItems
+    })
+  }
+
+  return (
+    <div>
+      <button onClick={handleAddItem}>Добавить элемент</button>
+      <button onClick={cleanQueue}>Очистить очередь</button>
+      <ol className="list flex flex-col w-[480px] gap-4">
+        {items.map((item, index) => (
+          <AnimatedListItem
+            key={item.id}
+            onDelete={() => deleteItem(index)}
+          >
+            number of list: {item.name}
+          </AnimatedListItem>
+        ))}
+        <div>В очереди: {queue.length}</div>
+      </ol>
+    </div>
+  )
+}
--- src/routes/categories.tsx
+++ src/routes/categories.tsx
@@ -1,5 +1,6 @@
 import { Link, Outlet } from 'react-router-dom'
 import { AnimatedList } from '../components/list/AnimatedList.tsx'
+import { AnimatedQueue } from '../components/animated-queue/AnimatedQueue.tsx'
 
 export default function CategoriesPage() {
   return (
@@ -7,6 +8,7 @@ export default function CategoriesPage() {
       <h1>Categories</h1>
 
       <AnimatedList />
+      <AnimatedQueue />
 
       <div>
         <Link to="/">Back home</Link>
