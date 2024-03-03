6ab51731a8c1ae986f87ec2b6a68b4095748e51c
Daniel <daniilterawars@gmail.com>
2024-03-03 18:41:34 +0300

    feature: env support

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,30 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { fileURLToPath, URL } from 'url'
 import react from '@vitejs/plugin-react-swc'
 import svg from '@neodx/svg/vite'
 
+const DEFAULT_PORT = 5173
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    svg({
-      root: 'src/assets',
-      output: 'public/sprites',
-      metadata: 'src/sprite.gen.ts',
-    }),
-  ],
-  server: {
-    port: 3000,
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(async ({ mode }) => {
+  const { PORT } = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      react(),
+      svg({
+        root: 'src/assets',
+        output: 'public/sprites',
+        metadata: 'src/sprite.gen.ts',
+      }),
+    ],
+    server: {
+      port: +PORT || DEFAULT_PORT,
     },
-  },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  }
 })
