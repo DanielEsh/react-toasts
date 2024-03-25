d46127505b06233a4ecee808c2236e4806d85e31
Daniel <daniilterawars@gmail.com>
2024-03-25 21:35:12 +0300

    feat(i18n): change lang

--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -3,13 +3,20 @@ import { BaseExample } from '@/pages/home-page/ui/base-example.tsx'
 import { PromiseExample } from '@/pages/home-page/ui/promise-example.tsx'
 import { HeadlessExample } from '@/pages/home-page/ui/headless-example.tsx'
 import { VariantsExample } from '@/pages/home-page/ui/variants-example.tsx'
+import { Button } from '@/shared/ui'
 
 export function HomePage() {
-  const { t } = useTranslation()
+  const { t, i18n } = useTranslation()
+
+  const changeLang = () => {
+    i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru')
+  }
 
   return (
     <div className="container">
-      {t('title')}
+      <div>{t('title')}</div>
+
+      <Button onClick={changeLang}>Change Lang</Button>
 
       <BaseExample />
 
