06b5bab71c061d7b5ff7899dcb567b85092c426a
Daniel <daniilterawars@gmail.com>
2024-03-26 21:51:36 +0300

    fix: i18n

--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -2,13 +2,10 @@ import { BaseExample } from '@/pages/home-page/ui/base-example.tsx'
 import { PromiseExample } from '@/pages/home-page/ui/promise-example.tsx'
 import { HeadlessExample } from '@/pages/home-page/ui/headless-example.tsx'
 import { VariantsExample } from '@/pages/home-page/ui/variants-example.tsx'
-import { LangChanger } from '@/modules/core/i18n/ui/lang-changer.tsx'
 
 export function HomePage() {
   return (
-    <div className="container">
-      <LangChanger />
-
+    <>
       <BaseExample />
 
       <VariantsExample />
@@ -16,6 +13,6 @@ export function HomePage() {
       <PromiseExample />
 
       <HeadlessExample />
-    </div>
+    </>
   )
 }
